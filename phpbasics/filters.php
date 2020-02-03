cbbbe75314fba35589f26cd5a51eabbb52c5ea83
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-02-03 22:30:59 +0000

    Second commit

--- phpbasics/filters.php
+++ phpbasics/filters.php
@@ -1,44 +1,82 @@
 <?php 
-    Check for posted data
-    if(filter_has_var(INPUT_POST, 'data')){
-        echo 'Data Found';
-    } else{
-        echo 'No Data';
-    }
+    // // Check for posted data
+    // if(filter_has_var(INPUT_POST, 'data')){
+    //     echo 'Data Found';
+    // } else{
+    //     echo 'No Data';
+    // }
 
-    // FILTER_VALIDATE_EMAIL
-    if(filter_has_var(INPUT_POST, 'data')){
-        if(filter_input(INPUT_POST, 'data', FILTER_VALIDATE_EMAIL)){
-            echo 'Email is valid';
-        } else {
-            echo 'Email is not valid';
-        }
-    }
+    // // FILTER_VALIDATE_EMAIL
+    // if(filter_has_var(INPUT_POST, 'data')){
+    //     if(filter_input(INPUT_POST, 'data', FILTER_VALIDATE_EMAIL)){
+    //         echo 'Email is valid';
+    //     } else {
+    //         echo 'Email is not valid';
+    //     }
+    // }
 
-    // FILTER_SANITIZE_EMAIL
-    if(filter_has_var(INPUT_POST, 'data')){
-        $email = $_POST['data'];
-        // Remove illegal chars
-        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-        echo $email.'<br>';
+    // // FILTER_SANITIZE_EMAIL
+    // if(filter_has_var(INPUT_POST, 'data')){
+    //     $email = $_POST['data'];
+    //     // Remove illegal chars
+    //     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //     echo $email.'<br>';
 
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-            echo 'Email is valid';
-        } else {
-            echo 'Email is not valid';
-        }
-    }
+    //     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+    //         echo 'Email is valid';
+    //     } else {
+    //         echo 'Email is not valid';
+    //     }
+    // }
 
-    // FILTER_VALIDATE_INT
-    $var = 42;
-    if(filter_var($var, FILTER_VALIDATE_INT)){
-        echo $var.' is a number';
-    } else {
-        echo $var.' is not a number';
-    }
+    // // FILTER_VALIDATE_INT
+    // $var = 42;
+    // if(filter_var($var, FILTER_VALIDATE_INT)){
+    //     echo $var.' is a number';
+    // } else {
+    //     echo $var.' is not a number';
+    // }
+
+
+    // $filters = array(
+    //     "data" => FILTER_VALIDATE_EMAIL,
+    //     "data2" => array(
+    //         "filter" => FILTER_VALIDATE_INT,
+    //         "options" => array(
+    //             "min_range" => 1,
+    //             "max_range" => 100
+    //         )
+    //     )
+    // );
+
+    // print_r(filter_input_array(INPUT_POST, $filters));
+
+    $arr = array(
+        "name" => "Liam Morgan",
+        "age" => "7",
+        "email" => "liamy2012@gmial.com"
+    );
+
+    $filters = array(
+        "name" => array(
+            "filter" => FILTER_CALLBACK,
+            "options" => "ucwords"
+        ),
+        "age" => array(
+            "filter" => FILTER_VALIDATE_INT,
+            "options" => array(
+                "min_range" => 1,
+                "max_range" => 120
+            )
+        ),
+        "email" => FILTER_SANITIZE_EMAIL
+    );
+
+    print_r(filter_var_array($arr, $filters));
+
 ?>
-
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <input type="text" name="data">
+    <input type="text" name="data2">
     <button type="submit">Submit</button>
 </form>

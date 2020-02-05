ba098ab25692b2d7649b7c59347fc14f502eef98
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-02-05 23:31:58 +0000

    Added validation

--- phpbasics/website3/index.php
+++ phpbasics/website3/index.php
@@ -1,17 +1,39 @@
 <?php
-   // Check for submit
-   if(filter_has_var(INPUT_POST, 'submit')){
-       // Get form data
-       $name = $_POST['name'];
-       $email = $_POST['email'];
-       $message = $_POST['message'];
-   }
+    // Message vars
+    $msg = '';
+    $msgClass = '';
+    
+    // Check for submit
+    if(filter_has_var(INPUT_POST, 'submit')){
+        // Get form data
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+        // Check required fileds
+        if(!empty($name) && !empty($email) && !empty($message)){
+            // Passed
+            // Check email
+            if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+                // Failed
+                $msg = 'Please use a valid email address';
+                $msgClass = 'alert-danger';
+            } else {
+                // Passed
+                echo 'PASSED';
+            }
+        } else {
+            // Failed
+            $msg = 'Please fill in all fields';
+            $msgClass = 'alert-danger';
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Contact Us</title>
-    <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.css">
 </head>
 <body>
     <nav class="navbar navbar-default">
@@ -22,18 +44,25 @@
         </div>
     </nav>
     <div class="container">
+        <?php if($msg != ''): ?>
+            <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+        <?php endif; ?>
         <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" name="name" class="form-control" value="">
+                <input type="text" name="name" class="form-control" 
+                value="<?php echo isset($_POST['name']) ? $name : '' ?>">
             </div>
             <div class="form-group">
                 <label>Email</label>
-                <input type="text" name="email" class="form-control" value="">
+                <input type="text" name="email" class="form-control" 
+                value="<?php echo isset($_POST['email']) ? $email : '' ?>">
             </div>
             <div class="form-group">
                 <label>Message</label>
-                <input type="text" name="message" class="form-control" value="">
+                <textarea name="message" class="form-control">
+                    <?php echo isset($_POST['message']) ? $message : ''; ?>    
+                </textarea>
             </div>
             <br>
             <button type="submit" name="submit" class="btn btn-primary">Submit</button>

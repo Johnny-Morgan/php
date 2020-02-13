82abeb2348adfa1304b748ebfbe25f916479b28f
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-02-13 23:56:18 +0000

    Initial commit

---
+++ phpbasics/website6/suggest.php
@@ -0,0 +1,22 @@
+<?php
+
+$people[] = "John";
+
+$q = $_REQUEST['q'];
+$suggestion = "";
+if($q != ""){
+    $q = strtolower($q);
+    $len = strlen($q);
+    foreach($people as $person){
+        if(stristr($q, substr($person, 0, $len))){
+            if($suggestion === ""){
+                $suggestion = $person;
+            } else {
+                $suggestion .= ", $person";
+            }
+        }
+    }
+}
+
+echo $suggestion === "" ? "No Suggestion" : $suggestion;
+?>

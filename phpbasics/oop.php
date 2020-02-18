261b36dcb172ea885bc93a91dedc20e3fe6a3d20
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-02-18 23:20:43 +0000

    Added Customer class

--- phpbasics/oop.php
+++ phpbasics/oop.php
@@ -2,6 +2,7 @@
     class Person {
         private $name;
         private $email;
+        public static $ageLimit = 40;
 
         public function __construct($name, $email) {
             $this -> name = $name;
@@ -18,7 +19,7 @@
         }
 
         public function getEmail() {
-            return $this -> email;
+            return $this -> email.'<br>';
         }
 
         public function setName($name) {
@@ -28,10 +29,36 @@
         public function setEmail() {
            $this -> email = $email;
         }
+
+        public static function getAgeLimit() {
+            return self::$ageLimit;
+        }
     }
 
-    $person1 = new Person('John Morgan', 'test@gmail.com');
-    echo $person1 -> getName();
-    
+    // $person1 = new Person('John Morgan', 'test@gmail.com');
+    // echo $person1 -> getName();
+    // echo Person::$ageLimit;
+    echo Person::getAgeLimit().'<br>';
+
+    class Customer extends Person {
+        private $balance;
+
+        public function __construct($name, $email, $balance) {
+            parent::__construct($name, $email, $balance);
+            $this -> balance = $balance;
+            echo 'A new '.__CLASS__.' has been created<br>';
+        }
+
+        public function getBalance() {
+            return $this -> balance.'<br>';
+        }
+
+        public function setBalance($balance) {
+            $this -> name = $balance;
+        }
+    }
+
+    $customer1 = new Customer('John Doe', 'jdoe@gmail.com', 1000);
+    echo $customer1 -> getBalance();
 
 ?>

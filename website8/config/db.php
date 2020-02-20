e3626cc92f431fa9b9245f3f9975c49e068eb992
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-02-20 01:14:38 +0000

    Deleted db.php

--- website8/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Create Connection
-    $conn = mysqli_connect('localhost', 'root', 'YES', 'phpblog');
-
-    // Check Connection
-    if(mysqli_connect_errno()) {
-        // Connection failed
-        echo 'Failed to connect to MySQL '.mysqli_connect_errno();
-    }

d4ed9d25b2738872a7d205732751f66db3ca98c0
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-02-27 01:20:55 +0000

    Code for submit button

--- phpbasics/website8/addpost.php
+++ phpbasics/website8/addpost.php
@@ -1,6 +1,22 @@
 <?php
     require('config/config.php');
     require('config/db.php');
+
+    // Check for Submit
+    if(isset($_POST['submit'])) {
+        // Get form data
+        $title = mysqli_real_escape_string($conn, $_POST['title']);
+        $body = mysqli_real_escape_string($conn, $_POST['body']);
+        $author = mysqli_real_escape_string($conn, $_POST['author']);
+        
+        $query = "INSERT INTO posts(title, author, body) VALUES ('$title', '$author', '$body')";
+        
+        if(mysqli_query($conn, $query)) {
+            header('Location: '.ROOT_URL.'');
+        } else {
+            echo 'ERROR: '. mysqli_error($conn);
+        }
+    }
 ?>
 
 <?php include('inc/header.php'); ?>
@@ -19,7 +35,7 @@
                 <label>Body</label>
                 <textarea name="body" class="form-control"></textarea>
             </div>
-            <input type="subnit" name="submit" value="submit">
+            <input type="submit" name="submit" value="Submit" class="btn btn-primary">
         </form>
     </div>
 <?php include('inc/footer.php'); ?>
--- phpbasics/website8/inc/navbar.php
+++ phpbasics/website8/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-default bg-light ">
+<nav class="navbar navbar-expand-md navbar-default bg-light">
   <a class="navbar-brand" href="#">PHPblog</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -6,9 +6,8 @@
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home <span class="sr-only">(current)</span></a>
-      </li>
+      <li><a class="nav-link" href="<?php echo ROOT_URL; ?>">Home</a></li>
+      <li><a class="nav-link" href="<?php echo ROOT_URL; ?>addpost.php">Add Post</a></li>
     </ul>
   </div>
 </nav>

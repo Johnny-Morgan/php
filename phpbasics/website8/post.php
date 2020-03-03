5396531ef32959e7bd8b545fa859151977f25a1b
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-03-03 22:27:25 +0000

    Added delete button

--- phpbasics/website8/post.php
+++ phpbasics/website8/post.php
@@ -2,6 +2,20 @@
     require('config/config.php');
     require('config/db.php');
 
+     // Check for Submit
+     if(isset($_POST['delete'])) {
+        // Get form data
+        $delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
+        
+        $query = "DELETE FROM posts WHERE id = {$delete_id}";
+        
+        if(mysqli_query($conn, $query)) {
+            header('Location: '.ROOT_URL.'');
+        } else {
+            echo 'ERROR: '. mysqli_error($conn);
+        }
+    }
+
     // Get ID
     $id = mysqli_real_escape_string($conn, $_GET['id']);
 
@@ -30,6 +44,10 @@
         by <?php echo $post['author'];?></small>
         <p><?php echo $post['body']; ?></p>
         <hr>
+        <form class="float-right" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <input type="hidden" name="delete_id" value="<?php echo $post['id']; ?>">
+            <input type="submit" name="delete" value="Delete" class="btn btn-danger">
+        </form>
         <a href="<?php echo ROOT_URL; ?>editpost.php?id=<?php echo $post['id']; ?>"
         class="btn btn-light">Edit</a>
     </div>
